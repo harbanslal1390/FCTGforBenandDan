93f7790b228c4ac3f2ecff7f2eb3825dc34b17a8
Admin <Admin@192.168.20.7>
2024-08-28 10:18:10 +1000

    Demo Created for Ben and Dan.

---
+++ FCTGWebAutomation/src/test/java/FCTG_HomePage/HomePage.java
@@ -0,0 +1,34 @@
+package FCTG_HomePage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import dev.failsafe.internal.util.Assert;
+
+public class HomePage {
+	public static WebDriver driver=new ChromeDriver();
+	public String abc;
+	@Test(priority = 1)
+		
+public void openBrowser()
+{
+	driver.get("https://secure.dc2.pageuppeople.com/apply/889/aw/applicationForm/default.asp?sData=UFUtVjMtQNgHEBGSrwozoQfMrYFnXkWfcuTYT03VUBJlYtC0Gu7Mh9dfDDv5b88QxW6TLTDIDAEhr0M9JQ%2BPYxz38lGxkVaATsRlsurZeYrOSZinWZB8eeYQD4zST%2FlT%2Bj8TasGvr1SwUUG%2F3kW%2BUlBwMkfQzA%3D%3D");
+	driver.manage().window().maximize();
+	abc= driver.findElement(By.xpath("//*[@id=\"loginPanel\"]/div[4]/div[1]/label")).getText();
+
+			
+	}
+	@Test (priority = 2)
+	public void verifyLabelName()
+	{
+		System.out.println(abc);
+		org.testng.Assert.assertEquals(abc, "Email address:1");
+	}
+	@Test (priority = 3)
+	public void closeBrowser()
+	{
+		driver.quit();
+	}
+}
